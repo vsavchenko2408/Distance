3435faada79031bf660715a7ce22a4aa031c0b3a
Dermeister <v.savchenko2408@gmail.com>
2024-08-23 13:04:31 +0300

    sync

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using std::cin;
 
 int main()
 {
-Distance d(4,5);
-
-d.show_distance();
-d.show_meter();
+Distance d1(4,5), d2(3,7);
+Distance d3 = d1*d2;
+d3.show_distance();
+d3.show_meter();
 
 return 0;
 }
